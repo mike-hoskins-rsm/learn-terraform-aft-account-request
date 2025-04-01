18b515f4b4069969c468e4cc152431f96e8e7995
EC2 Default User <ec2-user@ip-172-31-10-216.us-east-2.compute.internal>
2025-04-01 17:55:16 +0000

    creating acct3

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,7 +6,7 @@ module "acct2" {
     AccountName               = "acct2-aft"
     ManagedOrganizationalUnit = "Learn AFT"
     SSOUserEmail              = "mikehoskins@gmail.com"
-    SSOUserFirstName          = "Acct2"
+    SSOUserFirstName          = "Acct2-user"
     SSOUserLastName           = "AFT"
   }
 
@@ -24,4 +24,32 @@ module "acct2" {
   }
 
   account_customizations_name = "acct2"
+}
+
+module "acct3" {  
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "mikehoskins+aft2025-acct3@gmail.com"  
+    AccountName               = "acct3-aft"  
+    ManagedOrganizationalUnit = "Learn AFT"
+    SSOUserEmail              = "mikehoskins@gmail.com"
+    SSOUserFirstName          = "Acct3-user"
+    SSOUserLastName           = "AFT"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "acct3"
 }
